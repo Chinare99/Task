20eb5b903552a41e71c1d7d6c67bf529f0756138
Chinare99 <cinareismayilovaa.com>
2024-02-27 13:19:34 +0400

    T1

---
+++ 26.02 Ev Tapşırığı/Models/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
+
+namespace _26._02_Ev_Tapşırığı.Models
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            int[] grades = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, };
+        }
+
+    }
+}
---
+++ 26.02 Ev Tapşırığı/Models/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26._02_Ev_Tapşırığı.Models
+{
+    public class Student
+    {
+        int i;
+        int item;
+        public int[] Grades { get; set; }
+        public Student()
+        {
+            Grades = new int[0];
+            {
+
+                int[] grades = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, };
+                {   for (int i = 0; i < grades.Length; i++)
+                    {
+                        if (item % 2 == 0)
+                        {
+                            Grades[i] = grades[i];
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
